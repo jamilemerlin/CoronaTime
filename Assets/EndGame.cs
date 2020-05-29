91db437d6fa35e8708d1f54848ec32e21103394b
Jamile Merlin <jamilemerlin@gmail.com>
2020-05-29 20:41:08 +0200

    add restart

--- Assets/EndGame.cs
+++ Assets/EndGame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class EndGame : MonoBehaviour
 
 
 
-    void Start()
+    public void GameOver()
     {
         Text.gameObject.SetActive(false);
         SubText.gameObject.SetActive(false);
@@ -24,20 +25,27 @@ public class EndGame : MonoBehaviour
 
     void Update()
     {
-
+        if (IsDead && Input.GetKeyDown(KeyCode.Space))
+        {
+            Restart();
+        }
     }
+
     public void SetGame()
     {
-        if (!IsDead)
-        {
-            IsDead = true;
-            Text.gameObject.SetActive(true);
-            SubText.gameObject.SetActive(true);
-            GameOverScore.gameObject.SetActive(true);
-            Text.text = "Game Over";
-            SubText.text = "Press Enter to Try Again";
-            GameOverScore.text = "Score: " + Mathf.FloorToInt(scoreText.Timer).ToString("000");
-            scoreText.gameObject.SetActive(false);
-        }
+        IsDead = true;
+        Text.gameObject.SetActive(true);
+        SubText.gameObject.SetActive(true);
+        GameOverScore.gameObject.SetActive(true);
+        Text.text = "Game Over";
+        SubText.text = "Press Space to Try Again";
+        GameOverScore.text = "Score: " + Mathf.FloorToInt(scoreText.Timer).ToString("000");
+        scoreText.gameObject.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        IsDead = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
